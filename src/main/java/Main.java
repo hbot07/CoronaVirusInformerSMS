044ce4dcfad058696c0f8698b203e7e127f50952
Parth <hbot07@gmail.com>
2020-03-15 23:03:31 +0530

    resolved issue "make constants values configurable".

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Scanner;
 
 public class Main {
-
+    static Scanner sc=new Scanner(System.in);
     public static void main(String[] args) throws IOException {
         String st = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
         URL stockURL = new URL(st);
@@ -13,7 +14,9 @@ public class Main {
         int i=0;
         while(!(s=in.readLine()).contains("India")) {
         }
+        System.out.println("How frequesntly do you want to recieve the updates? Enter delay between two updates(in hours)");
+        int delay=sc.nextInt();
         while(true)
-            Scheduler.main("Number of confirmed cases in India: "+s.substring(s.lastIndexOf(",")+1));
+            Scheduler.main("Number of confirmed cases in India: "+s.substring(s.lastIndexOf(",")+1),delay);
     }
 }
