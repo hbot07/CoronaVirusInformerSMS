b39a576f8f923f2c11e959e2626a2d4f1b99aa21
Parth <hbot07@gmail.com>
2020-03-15 22:39:47 +0530

    Upload

--- src/main/java/PrivateInfo.java
+++ src/main/java/PrivateInfo.java
@@ -2,12 +2,12 @@ public class PrivateInfo {
     //This class was created to protect private information while the project code goes to github
     // To protect ACCOUNT_SID and AUTH_TOKEN
 
-    public static final String ACCOUNT_SID ="ACaa21b31833e380eee0df1609dd0ed8d6";
+    public static final String ACCOUNT_SID ="";
 
-    public static final String AUTH_TOKEN ="02685a6366632e3811246b237f43e36f";
+    public static final String AUTH_TOKEN ="";
 
-    public static String to="+918770951130";
+    public static String to="+";
 
-    public static String from="+12055649898";
+    public static String from="";
 
 }
